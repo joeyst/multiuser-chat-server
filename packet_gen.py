14133d857d5b371c6381d5c2980acbeca0380683
joeyst <joeystenbeck@gmail.com>
2022-12-04 22:45:22 -0800

    Remove functions.

--- packet_gen.py
+++ packet_gen.py
@@ -9,7 +9,6 @@ def dict_to_packet(**kwargs):
   return length + jsoned
 
 
-
 # Client packet generation 
 def get_client_chat_packet(msg):
   return dict_to_packet(type='chat', message=msg)
@@ -18,11 +17,12 @@ def get_client_hello_packet(nick):
   return dict_to_packet(type='hello', nick=nick)
 
 
-
 # Server packet generation 
 def get_server_chat_packet(msg, nick):
   return dict_to_packet(type='chat', nick=nick, message=msg)
 
+# Sorry for some confusing logic between 'join' and 'hello' here.... was a headache writing 
+# this and don't have the time to fix it. 
 def get_server_join_packet(nick):
   return dict_to_packet(type='join', nick=nick)
 
@@ -32,44 +32,11 @@ def get_server_leave_packet(nick):
 def get_server_error_packet():
   return dict_to_packet(type='error', msg='Unable to join server. Could be due to invalid nick name.')
 
-def is_client_hello(packet):
-  if (len(packet) < 2):
-    return False
-
-  json_dict, _ = pop_dict(packet)
-  try:
-    if (json_dict['type'] == 'hello'):
-      return True
-    else:
-      return False
-  except:
-    return False 
-
-def get_nick(hello_packet):
-  return pop_dict(hello_packet)[0]['nick']
-
-
-import json
-
-def _dict_to_packet(**kwargs):
-  jsoned = json.dumps(kwargs).encode('utf-8')
-  length = int.to_bytes(len(jsoned), 2, 'big')
-  return length + jsoned
-
-def get_client_chat_packet_from_message(msg):
-  return _dict_to_packet(type='chat', message=msg)
-
-def get_client_hello_packet_from_nick(nick):
-  return _dict_to_packet(type='hello', nick=nick)
-
 def get_server_chat_packet_from_message_and_nick(msg, nick):
-  return _dict_to_packet(type='chat', nick=nick, message=msg)
+  return dict_to_packet(type='chat', nick=nick, message=msg)
 
 def get_server_join_packet_from_nick(nick):
-  return _dict_to_packet(type='hello', nick=nick)
+  return dict_to_packet(type='hello', nick=nick)
 
 def get_server_leave_packet_from_nick(nick):
-  return _dict_to_packet(type='leave', nick=nick)
-
-def get_server_error_packet():
-  return _dict_to_packet(type='error', msg='Unable to join server. Could be due to invalid nick name.')
+  return dict_to_packet(type='leave', nick=nick)
