e886dd9ffcfc47e9f06a079a9d43866ef45f364c
joeyst <joeystenbeck@gmail.com>
2022-12-04 22:48:59 -0800

    Remove functions.

--- client_recv.py
+++ client_recv.py
@@ -2,6 +2,7 @@
 import json
 from packet_decode import is_poppable, pop 
 from messages import *
+from slice_buf import *
 
 # client_recv.py
 def display_messages(buf):
@@ -66,37 +67,6 @@ client_format_dict = {
   'hello': get_join_msg,
   'leave': get_leave_msg
 }
-
-def _get_length_from_first_two_bytes(buf):
-  return int.from_bytes(buf[0:2], 'big')
-
-def _get_remaining_buffer(buf):
-  length = _get_length_from_first_two_bytes(buf)
-  return buf[2+length:]
-
-def _get_slice_data(buf):
-  length = _get_length_from_first_two_bytes(buf)
-  return buf[2:2+length]
-
-def slice_buf(buf):
-  return _get_slice_data(buf), _get_remaining_buffer(buf)
-
-### Ensuring 
-length = int.to_bytes(12, 2, 'big')
-packet = length + b'abc'
-assert slice_buf(packet) == (b'abc', b'')
-
-packet = length + b'abcdefghijklmnopqrst'
-assert slice_buf(packet) == (b'abcdefghijkl', b'mnopqrst')
-
-import json
-
-def slice_buf_to_dict(buf):
-  data, buf = slice_buf(buf)
-  return json.loads(data), buf
-
-# buf_dict, name_dict, ready_set 
-
 
 def display_packet_from_dict(bdict):
   print(client_format_dict[bdict['type']](**bdict))
